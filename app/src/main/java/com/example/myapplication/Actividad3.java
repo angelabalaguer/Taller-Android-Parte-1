7f0d1b31696ea7bfa03b549f4dcc0b04966d9d84
unknown <balaguer.valles@gmail.com>
2019-11-30 11:57:07 -0500

    Taller Android

---
+++ app/src/main/java/com/example/myapplication/Actividad3.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Actividad3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad3);
+    }
+
+    public void actividad2(View view) {
+        startActivity(new Intent(this, Main2Activity.class));
+    }
+
+    public void MiSpinner(View view) {
+        startActivity(new Intent(this, MiSpinner.class));
+
+    }
+
+}
